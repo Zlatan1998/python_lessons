a1e7a7790369c6e53336c85fa9b0ffef3adb7bdb
Zlatan1998 <128599214+Zlatan1998@users.noreply.github.com>
2023-04-08 16:31:09 +0100

    add body_mass_index.py

---
+++ python_scripts/body_mass_index.py
@@ -0,0 +1,7 @@
+weight = float(input("Enter the value of weight: "))
+
+height = float(input("Enter the value of height: "))
+
+body_mass_index = weight / (height * height)
+
+print(body_mass_index)
