52059e7adbaec33e55802b91c405900d0726a003
Zlatan1998 <128599214+Zlatan1998@users.noreply.github.com>
2023-04-08 16:29:46 +0100

    Add area_of_a_trapezium.py

---
+++ python_scripts/area_of_a_trapezium.py
@@ -0,0 +1,5 @@
+abman = float(input("Enter the value of abman: "))
+baman = float(input("Enter the value of baman: "))
+height = float(input("Enter the value of height: "))
+area_of_a_trapezium = ((abman + baman)/2) * height
+print(area_of_a_trapezium)
