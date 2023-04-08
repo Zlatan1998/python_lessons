79c403688ad2bebe698643dc6d0cf037ed22d4b6
Zlatan1998 <128599214+Zlatan1998@users.noreply.github.com>
2023-04-08 16:34:46 +0100

    add formula_for_gideon.py

---
+++ python_scripts/formula_for_gideon.py
@@ -0,0 +1,5 @@
+height = float(input("Enter the value of height: "))
+base = float(input("Enter the value of base: "))
+mass = float(input("Enter the value of mass: "))
+formula_for_gideon = height * base * mass
+print(formula_for_gideon)
