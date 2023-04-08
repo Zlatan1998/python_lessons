6dfb57c0d52083edc4950bf91ff8c44007892111
Zlatan1998 <128599214+Zlatan1998@users.noreply.github.com>
2023-04-08 16:35:36 +0100

    add simple_interest.py

---
+++ python_scripts/simple_interest.py
@@ -0,0 +1,5 @@
+principal = float(input("Enter the value of principal: "))
+rate = float(input("Enter the value of rate: "))
+time = float(input("Enter the value of time: "))
+simple_interest = principal * rate * time
+print(simple_interest)
