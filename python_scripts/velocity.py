70df3570736bbb5d7d7a6f1679dbb33b9d7d6165
Zlatan1998 <128599214+Zlatan1998@users.noreply.github.com>
2023-04-08 16:35:57 +0100

    add velocity.py

---
+++ python_scripts/velocity.py
@@ -0,0 +1,5 @@
+initial_velocity = float(input("Enter the value of initial velocity: "))
+acceleration = float(input("Enter the value of acceleration: "))
+time_of_motion = float(input("Enter the value of time of motion: "))
+velocity = initial_velocity + acceleration * time_of_motion
+print(velocity)
